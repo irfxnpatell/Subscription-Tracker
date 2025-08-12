6bd12278f8b3f19fe81516e76778fc38bd983dd3
irfxnpatell <irfanpatell2004@gmail.com>
2025-08-12 17:40:04 +0530

    middleware updation

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errormiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+
+app.use(errormiddleware);
 
 app.get("/", (req,res) => {
     res.send("Welcome to Subscription Tracker API ");
